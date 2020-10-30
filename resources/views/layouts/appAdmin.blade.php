b8e96c4265a8a3dfdcf7947fe8e23a0e6b3cc76b
FeinardSlim <58687783+FeinardSlim@users.noreply.github.com>
2020-10-30 16:19:30 +0700

    update admin header

--- resources/views/layouts/appAdmin.blade.php
+++ resources/views/layouts/appAdmin.blade.php
@@ -6,6 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+      <link rel="shortcut icon" href="assets/images/sisco-128x128-1.png" type="image/x-icon">
 
     <title>{{config('SISCO')}}</title>
 
@@ -16,87 +17,82 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
-    <div id="app">
-        <main class="py-4"><div id="app">
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ url('/admin') }}">
-                        SISCO
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+    
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+  <div class="container">
+      <a class="navbar-brand" href="{{ url('/admin') }}">
+          {{ config('app.name', 'SISCO') }}
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          <span class="navbar-toggler-icon"></span>
+      </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Left Side Of Navbar -->
+          <ul class="navbar-nav mr-auto">
 
-                        </ul>
+          </ul>
 
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/ambulans">Ambulance</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/artikels">Artikel</a>
-                            </li>                            
-                            <li class="nav-item">
-                                <a class="nav-link" href="/laporans/create">Posko Kesehatan</a>
-                            </li>
-                            {{-- <li class="nav-item">
-                                <a class="nav-link" href="/laporans/create">Laporan</a>
-                            </li>                             --}}
-                            <li class="nav-item">
-                                <a class="nav-link" href="/relawan">Relawan</a>
-                            </li>
-                        </ul>
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                  <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+                  <a class="nav-link" href="/artikels">Artikel</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/laporans/create">Laporan</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/ambulan/create">Ambulan</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/relawan">Relawan</a>
+            </li>
+          </ul>
 
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                            <!-- Authentication Links -->
-                            @guest
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                                {{-- @if (Route::has('register'))
-                                    <!--<li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                    </li>-->
-                                @endif --}}
-                            @else
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }} <span class="caret"></span>
-                                    </a>
+          <!-- Right Side Of Navbar -->
+          <ul class="navbar-nav ml-auto">
+              <!-- Authentication Links -->
+              @guest
+                  <li class="nav-item">
+                      <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                  </li>
+              @else
+                  <li class="nav-item dropdown">
+                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                          {{ Auth::user()->name }} <span class="caret"></span>
+                      </a>
 
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="../admin">Admin</a>
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                            document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
+                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="/dashboard">Dashboard</a>
+                          <a class="dropdown-item" href="{{ route('logout') }}"
+                              onclick="event.preventDefault();
+                                              document.getElementById('logout-form').submit();">
+                              {{ __('Logout') }}
+                          </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                            @endguest
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            @include('inc.messages')
-            @yield('content')
-        </main>
-    </div>
+                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                              @csrf
+                          </form>
+                      </div>
+                  </li>
+              @endguest
+          </ul>
+      </div>
+  </div>
+</nav>
+    <main class="container">
+        @include('inc.messages')
+        @yield('content')
+    </main>
+    {{-- </div> --}}
+    <script src="{{asset('/vendor/unisharp/laravel-ckeditor/ckeditor.js')}}"></script>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
 </body>
 </html>
